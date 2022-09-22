463fa3e07533107e4f17651f2abe16d4895715ef
s13bp <86206902+s13bp@users.noreply.github.com>
2022-09-23 00:47:02 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = io('https://test-4e7w.vercel.app/')
+const socket = io('https://test-4e7w.vercel.app:3000')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
